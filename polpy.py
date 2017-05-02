255501548bb256b04d43c5020b046f63bf3dd4ce
Sasha <sasha@Sashas-iMac.local>
2017-05-01 23:51:46 -0700

    updateeee

--- polpy.py
+++ polpy.py
@@ -4,9 +4,12 @@
 import sqlite3
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 import talib
 from poloniex import Poloniex
-import matplotlib.pyplot as plt
+from time import time
+import sys 
+
 
 conn = sqlite3.connect('example.db')
 c = conn.cursor()
@@ -16,45 +19,69 @@ polo = Poloniex()
 # private 
 #polo = poloniex.Poloniex('your-Api-Key-Here-xxxx','yourSecretKeyHere123456789')
 
-def dbInit(currencyPair):
-	
-	sql = 'CREATE TABLE IF NOT EXISTS ' + currencyPair + ' (volume real, quoteVolume real, high real, low real, date text, close real, weightedAverage real, open real)'
-	c.execute(sql)
 
-def dbInsert(currencyPair):
-	
-	temp = polo.returnChartData(currencyPair) #returnChartData(self, pair, period=False, start=False, end=False)
-	for data in temp:
-		sql = 'INSERT INTO ' +currencyPair + ' VALUES (?,?,?,?,?,?,?,?)'
-		#convert from unicode to float
-		c.execute(sql, [data['volume'], data['quoteVolume'], data['high'], data['date'], data['low'], data['close'], data['weightedAverage'], data['open']])
+
+class Crypto(object):
+
+	def __init__(self, currencyPair,  startDate=False, interval=False, endDate = False):
+
+		self.currencyPair = currencyPair
+		if interval not in [300, 900, 1800, 7200, 14400, 86400]:
+			self.interval = 86400 # day 
+		else:
+			self.interval = interval
+		if not startDate:
+			startDate = time() - 2592000 # month
+		if not endDate:
+			endDate = time() 
+			self.lastDate = endDate
+		self.data = polo.returnChartData(self.currencyPair, self.interval, startDate, self.lastDate) #returnChartData(self, pair, period=False, start=False, end=False)
+
+
+	def update(self):
+		currentDate = time()
+		self.data.extend(polo.returnChartData(self.currencyPair, self.interval, self.lastDate, currentDate))
+		self.lastDate = currentDate
+
+		return self.data
+
+	def avgArray(self):
+		self.avg = []
+		for x in range(0,len(self.data)):
+			self.avg.append(float(self.data[x]['weightedAverage']))
+		self.avg = np.asarray(self.avg)
+		return self.avg
+
+
+	def calcEMA(self,interval):
+		out = talib.EMA(self.avg,interval)
+		self.EMA= out
+		return self.EMA
+
+	def calcMACD(self):
+
+		fastperiod = 12
+		slowperiod = 26
+		signalperiod = 9
+		macd, signal, hist = talib.MACD(self.avg,fastperiod, slowperiod, signalperiod)
 		
-	conn.commit()	
-	#conn.close()
+		self.MACD = macd - signal
+		#print(self.MACD)
+		#return self.macd
 
-def calcEMA(currencyPair):
-	
-	sql = 'SELECT weightedAverage FROM ' + currencyPair
-	c.execute(sql)
-	data = c.fetchall() # data is returned as a tuple
-	for x in range(0,len(data)):
-		data[x] = data[x][0]
-	#print(data)
-	dataNP = np.asarray(data) # cast as numpy array 
-	out = talib.EMA(dataNP,15)
-	print(np.shape(dataNP))
-	x = list(range(0,len(data)))
-	# plt.plot(list(x, data,'r',x , out, 'g')
-	# plt.show()
-	plt.plot(x, data, 'g', x, out, 'r')
-	plt.xlabel('30 day window')
-	plt.ylabel('BTC/ETH price')
-	plt.show()
-
-
-	#print(type(data[1]))
-	#floatData = [float(x) for x in data] #talib no likey non float variables 	
-	#out = talib.SMA(floatData,15)
-
-
+	def plot(self,data,algo1, algo2):#*args,algo2):
+		
+		
+		x = range(0,len(self.data))
+		plt.plot(x, data, 'g')
+		plt.plot(x, algo1)
+		plt.plot(x, algo2)
+		# for y in args:
+		# 	plt.plot(x,y)
+		idx = np.argwhere(np.diff(np.sign( algo1 - algo2)) != 0)#.reshape(-1) + 0
+		for intersection in idx:
+			plt.plot(x[intersection], algo2[intersection], 'ro')
+		plt.xlabel('Time')
+		plt.ylabel('BTC/ETH price')
+		plt.show()
 
--- run.py
+++ run.py
@@ -1,6 +1,26 @@
 import polpy
+import numpy as np
+c = polpy.Crypto('BTC_ETH',0,1800,0)
+a= c.update()
+q = c.avgArray()
+b =c.calcEMA(5)
+e = c.calcEMA(10)
 
-pair = raw_input('Enter Currency Pair (ex BTC_ETH):  ')
-polpy.dbInit(pair)
-polpy.dbInsert(pair)
-polpy.calcEMA(pair)
+
+
+w = c.plot(q,b,e)
+
+
+# import numpy as np
+# import matplotlib.pyplot as plt
+
+# x = np.arange(0, 1000)
+# f = np.arange(0, 1000)
+# #g = np.sin(np.arange(0, 10, 0.01) * 2) * 1000
+
+# plt.plot(x, f, '-')
+# #plt.plot(x, g, '-')
+
+# #idx = np.argwhere(np.diff(np.sign(f - g)) != 0).reshape(-1) + 0
+# #plt.plot(x[idx], f[idx], 'ro')
+# plt.show()
