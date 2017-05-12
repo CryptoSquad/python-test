48b310d82095b7b8124aff575998d8e8d4aa6ae2
Alejandro Ramirez <aramirez@SJAKM.local>
2017-05-12 16:20:52 -0700

    more tests

--- polpy.py
+++ polpy.py
@@ -1,7 +1,6 @@
 # using python poloniex wrapper
 # install with : pip install https://github.com/s4w3d0ff/python-poloniex/archive/v0.3.6.zip 
 
-import sqlite3
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -11,9 +10,6 @@ from time import time
 import time as t
 import sys 
 
-
-conn = sqlite3.connect('example.db')
-c = conn.cursor()
 #public
 polo = Poloniex()
 
@@ -32,101 +28,103 @@ class Crypto(object):
 		else:
 			self.interval = interval
 		if not startDate:
-			startDate = time() - 2592000 # month
+			startDate = time() - 1800 # month
 		if not endDate:
 			self.lastDate = time() 
 		else:
 			self.lastDate = endDate
-		self.data = polo.returnChartData(self.currencyPair, self.interval, startDate, self.lastDate) #returnChartData(self, pair, period=False, start=False, end=False)
-
+		self.data = pd.DataFrame(polo.returnChartData(self.currencyPair, self.interval, startDate, self.lastDate)) #returnChartData(self, pair, period=False, start=False, end=False)
+		# date high low open close volume quoteVolume weightedAverage+
 
 	def update(self):
 		currentDate = time()
-		self.data.extend(polo.returnChartData(self.currencyPair, self.interval, self.lastDate, currentDate))
+		temp = pd.DataFrame(polo.returnChartData(self.currencyPair, self.interval, self.lastDate, currentDate))#self.data.append(pd.DataFrame(polo.returnChartData(self.currencyPair, self.interval, self.lastDate, currentDate)))
 		self.lastDate = currentDate
-
-		return self.data
-
-	def avgArray(self):
-		self.avg = []
-		for x in range(0,len(self.data)):
-			self.avg.append(float(self.data[x]['weightedAverage']))
-		self.avg = np.asarray(self.avg)
-		return self.avg
-
-	def timeArray(self):
-		self.date = []
-		for x in range(0,len(self.data)):
-			self.date.append(float(self.data[x]['date']))
+		#self.data.append(self.data2)
+		frames = [self.data, temp]
+		self.data = pd.concat(frames)
 
 
-	def calcEMA(self,interval):
-		out = talib.EMA(self.avg,interval)
-		self.EMA= out
-		return self.EMA
+	# def avgArray(self):
+	# 	self.avg = []
+	# 	for x in range(0,len(self.data)):
+	# 		self.avg.append(float(self.data[x]['weightedAverage']))
+	# 	self.avg = np.asarray(self.avg)
+	# 	return self.avg
 
-	def calcMACD(self,fastPeriod,slowPeriod):
+	# def timeArray(self):
+	# 	self.date = []
+	# 	for x in range(0,len(self.data)):
+	# 		self.date.append(float(self.data[x]['date']))
 
 
-		macd, signal, hist = talib.MACD(self.avg,fastperiod, slowperiod, signalperiod)
+	# def calcEMA(self,interval):
+	# 	out = talib.EMA(self.avg,interval)
+	# 	self.EMA= out
+	# 	return self.EMA
+
+	# def calcMACD(self,fastPeriod,slowPeriod):
+
+
+	# 	macd, signal, hist = talib.MACD(self.avg,fastperiod, slowperiod, signalperiod)
 		
-		self.MACD = macd - signal
-		#print(self.MACD)
-		#return self.macd
+	# 	self.MACD = macd - signal
+	# 	#print(self.MACD)
+	# 	#return self.macd
 
-	def plot(self,data,algo1, algo2):#*args,algo2):
+	# def plot(self,data,algo1, algo2):#*args,algo2):
 		
 		
-		x = range(0,len(self.data))
-		plt.plot(x, data, 'g')
-		plt.plot(x, algo1)
-		plt.plot(x, algo2)
-		# for y in args:
-		# 	plt.plot(x,y)
-		self.idx = np.argwhere(np.diff(np.sign( algo1 - algo2)) != 0).reshape(-1) + 0
-		for intersection in self.idx:
-			plt.plot(x[intersection], algo2[intersection], 'ro')
-		plt.xlabel('Time')
-		plt.ylabel(self.currencyPair)
-		print 'Done'
-		#plt.show()
+	# 	x = range(0,len(self.data))
+	# 	plt.plot(x, data, 'g')
+	# 	plt.plot(x, algo1)
+	# 	plt.plot(x, algo2)
+	# 	# for y in args:
+	# 	# 	plt.plot(x,y)
+	# 	self.idx = np.argwhere(np.diff(np.sign( algo1 - algo2)) != 0).reshape(-1) + 0
+	# 	for intersection in self.idx:
+	# 		plt.plot(x[intersection], algo2[intersection], 'ro')
+	# 	plt.xlabel('Time')
+	# 	plt.ylabel(self.currencyPair)
+	# 	print 'Done'
+	# 	plt.show()
 
-	def backtest(self,algo1,algo2):
-		eth = 10
-		holding = True
-		trades = 0
-		x = range(0,len(self.data))
-		for i,j in enumerate(self.idx):
-			# print(j)
-			# print(algo1[j])
+	# def backtest(self,algo1,algo2):
+	# 	eth = 10
+	# 	holding = True
+	# 	trades = 0
+	# 	x = range(0,len(self.data))
+	# 	for i,j in enumerate(self.idx):
+	# 		# print(j)
+	# 		# print(algo1[j])
 
-			if algo1[j-2] > algo2[j-2] and holding: # check that faster algo is above slower
-				#buy
-				btc = eth * self.avg[j]
-				print('SELL')
-				date = t.ctime(self.date[j])
-				print(str(eth) + ' eth sold at ' + str(round(self.avg[j],5)))
-				print(date)
-				holding = False
-				trades = trades + 1
-				eth = 0
-				print('btc value- %f' % btc)
-				print('eth value- %f' % eth)
-				print('\n')
-			elif not holding:
-				eth = btc / self.avg[j]
-				print('BUY')
-				profit = (eth/10 - 1) * 100
-				date = t.ctime(self.date[j])
-				print(str(eth) + ' eth bought at ' + str(round(self.avg[j],5)))
-				print(date)
-				print('profit = ' + str(profit) + '% ')
-				holding = True
-				trades = trades + 1 
-				btc = 0
-				print('btc value- %f' % btc)
-				print('eth value- %f' % eth) 
-				print('\n')
-		print 'total trades = %i' % trades
+	# 		if algo1[j-2] > algo2[j-2] and holding: # check that faster algo is above slower
+	# 			#buy
+	# 			btc = eth * self.avg[j]
+	# 			print('SELL')
+	# 			date = t.ctime(self.date[j])
+	# 			print(str(eth) + ' eth sold at ' + str(round(self.avg[j],5)))
+	# 			print(date)
+	# 			holding = False
+	# 			trades = trades + 1
+	# 			eth = 0
+	# 			print('btc value- %f' % btc)
+	# 			print('eth value- %f' % eth)
+	# 			print('\n')
+	# 		elif not holding:
+	# 			eth = btc / self.avg[j]
+	# 			print('BUY')
+	# 			profit = (eth/10 - 1) * 100
+	# 			date = t.ctime(self.date[j])
+	# 			print(str(eth) + ' eth bought at ' + str(round(self.avg[j],5)))
+	# 			print(date)
+	# 			print('profit = ' + str(profit) + '% ')
+	# 			holding = True
+	# 			trades = trades + 1 
+	# 			btc = 0
+	# 			print('btc value- %f' % btc)
+	# 			print('eth value- %f' % eth) 
+	# 			print('\n')
+	# 	print 'total trades = %i' % trades
 
 
--- run.py
+++ run.py
@@ -1,85 +1,34 @@
 import polpy
 import numpy as np
+import pandas as pd
 from time import time
 import time as t
 import matplotlib.pyplot as plt
 import sys
 
-f = open('output.txt', 'w')
-sys.stdout = f
-start = time() - 12*2592000
-end = time() - 6*2592000
+
+start = time() - 5*1800
+end = time() - 3*1800
 c = polpy.Crypto('BTC_ETH',start,300,end)
-t.sleep(1) # wait so not to overload the API
-a= c.update()
-c.timeArray()
-q = c.avgArray()
-fast =c.calcEMA(5)
-slow = c.calcEMA(10)
+ # wait so not to overload the API
+c.update()
+# c.timeArray()
+# q = c.avgArray()
+# fast =c.calcEMA(10)
+# slow = c.calcEMA(20)
 
 
 
-w = c.plot(q,fast,slow)
-c.backtest(fast,slow)
+# df.drop(df.tail(1).index)
+#c.data.plot(x = 'date', y = 'weightedAverage', style = 'o')
 
-f.close()
-# import numpy as np
-# import matplotlib.pyplot as plt
+c.data.weightedAverage = c.data.weightedAverage.astype('float64')
 
-# x = np.arange(0, 1000)
-# f = np.arange(0, 1000)
-# #g = np.sin(np.arange(0, 10, 0.01) * 2) * 1000
 
-# plt.plot(x, f, '-')
-# #plt.plot(x, g, '-')
+for a in range(0,len(c.data.date),1):
+	c.data.date.iloc[a] = t.ctime(c.data.date.iloc[a])
 
-# #idx = np.argwhere(np.diff(np.sign(f - g)) != 0).reshape(-1) + 0
-# #plt.plot(x[idx], f[idx], 'ro')
+print c.data.date
+# c.data.plot(x = 'date', y = 'weightedAverage', style = 'o')
+
 # plt.show()
-
-
-'''
-def backtest(avg,fast,slow):
-
-	eth = 10
-	z = []
-	e = 0
-	print(type(z))
-	holding = True
-	for i,j in enumerate(q):
-
-		if 	fast[i] > 0 and round(fast[i],4) == round(slow[i],4):
-			if fast[i-2] > slow[i-2] and holding:
-				btc = eth * avg[i]
-				eth = 0
-				print('SELL')
-				print('btc value ', btc)
-				print('eth value ', eth)
-				holding = False
-				z.append(i)
-			elif not holding:
-				eth = btc / avg[i]
-				btc = 0
-				print('BUY')
-				print('btc value ', btc)
-				print('eth value ',eth)
-				profit = (eth/10 - 1) * 100
-				print('profit = ' + str(profit) + '% ')
-				holding = True 
-				z.append(i)
-	return z
-
-
-test = backtest(q,fast,slow)
-
-x = range(0,len(q))
-plt.plot(x, q, 'g')
-plt.plot(x, fast)
-plt.plot(x, slow)
-
-for intersection in test:
-	plt.plot(x[intersection], fast[intersection], 'ro')
-plt.xlabel('Time')
-
-plt.show()
-'''
